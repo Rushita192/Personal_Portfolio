40b7d4cd169acc6ac8473983b0cd6d019613bfe4
Rushita Patel <84351315+Rushita192@users.noreply.github.com>
2021-05-30 11:42:55 +0530

    Add files via upload

---
+++ route/router.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    //res.send('Hello Node JS');
+    res.render('index.ejs');
+});
+
+router.get('/about', (req, res) => {
+    //res.send('Hello Node JS');
+    res.render('about.ejs');
+});
+
+router.get('/contact', (req, res) => {
+    //res.send('Hello Node JS');
+    res.render('contact.ejs');
+});
+
+router.get('/project', (req, res) => {
+    //res.send('Hello Node JS');
+    res.render('project.ejs');
+});
+
+router.get('/service', (req, res) => {
+    //res.send('Hello Node JS');
+    res.render('service.ejs');
+});
+
+
+
+module.exports = router;
